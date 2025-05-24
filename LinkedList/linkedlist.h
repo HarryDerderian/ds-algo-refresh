a44233ab52662b00d497f026f42ea4f39e6565f6
Harry <harryderderian@csu.fullerton.edu>
2025-05-24 14:44:29 -0700

    header file

---
+++ LinkedList/linkedlist.h
@@ -0,0 +1,41 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+template <typename T>
+struct node
+{
+    node *next;
+    T data;
+};
+
+template <typename T>
+class LinkedList
+{
+
+public:
+    LinkedList();
+
+    LinkedList(T data);
+
+    ~LinkedList();
+
+    bool isEmpty() const;
+
+    int getSize() const;
+
+    void add(T data);
+
+    bool contains(T data) const;
+
+    T &get(int index) const;
+
+    void clear();
+
+    void remove(T data);
+
+private:
+    node<T> *head;
+    node<T> *tail;
+    int size;
+};
+#endif
