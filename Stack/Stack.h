686749546493493aa341cf37958051a0864d4831
Harry <harryderderian@csu.fullerton.edu>
2025-06-17 07:34:43 -0700

    starting stack

---
+++ Stack/Stack.h
@@ -0,0 +1,30 @@
+#ifndef STACK_H
+#define STACK_H
+
+template <typename T>
+class Stack
+{
+public:
+    Stack();
+
+    void push(const T &value);
+
+    T &peek();
+
+    T &pop();
+
+    bool contains(const T &value) const;
+
+    int size() const;
+
+    bool isEmpty() const;
+
+private:
+    void resize();
+
+    int arraySize;
+    T *array;
+    int topIndex;
+};
+
+#endif
